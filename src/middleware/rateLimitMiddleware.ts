d273fcfeefbf232024b14050e152cbf6933c9993
ibraheemtarek <ibraheemtarek99@yahoo.com>
2025-06-22 12:39:26 +0300

    increase login rate limiter

--- src/middleware/rateLimitMiddleware.ts
+++ src/middleware/rateLimitMiddleware.ts
@@ -12,7 +12,7 @@ export const apiLimiter = rateLimit({
 // More strict rate limiter for authentication routes
 export const authLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, 
-  limit: process.env.NODE_ENV === 'production' ? 10 : 100, // Higher limit for development
+  limit: process.env.NODE_ENV === 'production' ? 100 : 1000,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
   message: { message: 'Too many login attempts, please try again later.' },
